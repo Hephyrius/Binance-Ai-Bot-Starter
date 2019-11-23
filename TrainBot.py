e290056ba1d0aba627e2f16e51f53f78bc5b75d0
Harnick Khera <Hephyrius@gmail.com>
2019-11-23 04:25:32 +0000

    data dl

--- TrainBot.py
+++ TrainBot.py
@@ -26,79 +26,79 @@ api_key = '0'
 api_secret = '0'
 client = Client(api_key, api_secret)
 
-candles = client.get_historical_klines("BTCUSDT", Client.KLINE_INTERVAL_1HOUR, "01 Jan, 2017", "10 Jul, 2020")
+candles = client.get_historical_klines("BTCUSDT", Client.KLINE_INTERVAL_5MINUTE, "01 Jan, 2017", "10 Jul, 2020")
 
-#Convert the raw data from the exchange into a friendlier form with some basic feature creation
-x = cf.FeatureCreation(candles)
-
-#Create our targets
-y = cf.CreateTargets(candles,1)
-
-#remove the top elements of the features and targets - this is for certain features that arent compatible with the top most
-#for example SMA27 would have 27 entries that would be incompatible/incomplete and would need to be discarded
-y = y[94:]
-x = x[94:len(candles)-1]
-
-#produce sets, avoiding overlaps!
-#data is seporated temporily rather than randomly
-#this prevents the model learning stuff it wouldnt know - aka leakage - which can give us false positive models
-trny = y[:9999]
-trnx = x[:9999]
-
-#Validation set is not used in this starter model, but should be used if using other libraries that support early stopping.
-valy = y[10000:12999]
-valx = x[10000:12999]
-
-tsty = y[13000:]
-tstx = x[13000:]
-
-model = GradientBoostingClassifier() 
-model.fit(trnx,trny)
-
-preds = model.predict(tstx)
-
-#Some basic tests so we know how well our model performs on unseen - "modern" data.
-#Helps with fine tuning features and model parameters
-accuracy = accuracy_score(tsty, preds)
-mse = mean_squared_error(tsty, preds)
-
-print("Accuracy = " + str(accuracy))
-print("MSE = " + str(mse))
-
-falsePos = 0
-falseNeg = 0
-truePos = 0
-trueNeg = 0
-total = len(preds)
-
-for i in range(len(preds)):
-    
-    if preds[i] == tsty[i] and tsty[i] == 1:
-        truePos +=1
-        
-    elif preds[i] == tsty[i] and tsty[i] == 0:
-        trueNeg +=1
-        
-    elif preds[i] != tsty[i] and tsty[i] == 1:
-        falsePos +=1
-        
-    elif preds[i] != tsty[i] and tsty[i] == 0:
-        falseNeg +=1
-        
-print("False Pos = " + str(falsePos/total))
-print("False Neg = " + str(falseNeg/total))
-print("True Pos = " + str(truePos/total))
-print("True Neg = " + str(trueNeg/total))
-
-#how important of the features - helps with feature selection and creation!
-results = pd.DataFrame()
-results['names'] = trnx.columns
-results['importance'] = model.feature_importances_
-print(results.head)
-
-
-#save our model to the system for use in the bot
-dump(model, open("Models/model.mdl", 'wb'))
+##Convert the raw data from the exchange into a friendlier form with some basic feature creation
+#x = cf.FeatureCreation(candles)
+#
+##Create our targets
+#y = cf.CreateTargets(candles,1)
+#
+##remove the top elements of the features and targets - this is for certain features that arent compatible with the top most
+##for example SMA27 would have 27 entries that would be incompatible/incomplete and would need to be discarded
+#y = y[94:]
+#x = x[94:len(candles)-1]
+#
+##produce sets, avoiding overlaps!
+##data is seporated temporily rather than randomly
+##this prevents the model learning stuff it wouldnt know - aka leakage - which can give us false positive models
+#trny = y[:9999]
+#trnx = x[:9999]
+#
+##Validation set is not used in this starter model, but should be used if using other libraries that support early stopping.
+#valy = y[10000:12999]
+#valx = x[10000:12999]
+#
+#tsty = y[13000:]
+#tstx = x[13000:]
+#
+#model = GradientBoostingClassifier() 
+#model.fit(trnx,trny)
+#
+#preds = model.predict(tstx)
+#
+##Some basic tests so we know how well our model performs on unseen - "modern" data.
+##Helps with fine tuning features and model parameters
+#accuracy = accuracy_score(tsty, preds)
+#mse = mean_squared_error(tsty, preds)
+#
+#print("Accuracy = " + str(accuracy))
+#print("MSE = " + str(mse))
+#
+#falsePos = 0
+#falseNeg = 0
+#truePos = 0
+#trueNeg = 0
+#total = len(preds)
+#
+#for i in range(len(preds)):
+#    
+#    if preds[i] == tsty[i] and tsty[i] == 1:
+#        truePos +=1
+#        
+#    elif preds[i] == tsty[i] and tsty[i] == 0:
+#        trueNeg +=1
+#        
+#    elif preds[i] != tsty[i] and tsty[i] == 1:
+#        falsePos +=1
+#        
+#    elif preds[i] != tsty[i] and tsty[i] == 0:
+#        falseNeg +=1
+#        
+#print("False Pos = " + str(falsePos/total))
+#print("False Neg = " + str(falseNeg/total))
+#print("True Pos = " + str(truePos/total))
+#print("True Neg = " + str(trueNeg/total))
+#
+##how important of the features - helps with feature selection and creation!
+#results = pd.DataFrame()
+#results['names'] = trnx.columns
+#results['importance'] = model.feature_importances_
+#print(results.head)
+#
+#
+##save our model to the system for use in the bot
+#dump(model, open("Models/model.mdl", 'wb'))
 
 
 
